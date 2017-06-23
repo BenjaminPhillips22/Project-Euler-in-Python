caf36bd5d2a9f6f957845dc2d240cd70797fe822
Ben <benjamin.phillips@silverpond.com.au>
2017-06-23 16:49:40 +1000

    commit

---
+++ p24.py
@@ -0,0 +1,22 @@
+#The millionth lexicographic permutation is 2783915460
+#Time: 0.0005 seconds
+
+import time
+import os
+import math
+
+start = time.time()
+
+def nthPerm(s, n):
+    if len(s)==1:
+        return s
+    k = math.factorial(len(s)-1)
+    div = n//k
+    mod = n%k
+    return s[div] + nthPerm(s[:div] + s[div+1:], mod)
+
+
+
+print('The millionth lexicographic permutation is ' + str(nthPerm('0123456789', 999999)) )
+elapsed = (time.time() - start)
+print("Time: " + str(round(elapsed, 4)) + " seconds")
