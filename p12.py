61fa62dee077886f4b53a58eec0b39314adf8695
Ben <benjamin.phillips@silverpond.com.au>
2017-10-11 21:48:15 +1100

    list comprehension

--- p12.py
+++ p12.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun 30 22:38:10 2016
-
+Modified on Wed Oct 11 2017
 @author: ben
 
 76576500 is the first triangle number to have over 500 factors
@@ -12,24 +12,23 @@ Time: 6.08 seconds
 import time
 import math
 
+
 def factors(n):
-    result = []
-    for i in range(1, math.floor(math.sqrt(n))):
-        if n % i == 0:
-            result.append(i)
+    result = [i for i in range(1, math.floor(math.sqrt(n))) if n % i == 0]
     return result
-    
+
+
 start = time.time()
-    
+
 j = 1
 num = 0
-test = True
-while test:
+while True:
     num += j
     j += 1
     if len(factors(num))*2 > 500:
-        print(str(num) + " is the first triangle number to have over 500 factors")
+        print(str(num),
+              " is the first triangle number to have over 500 factors")
         break
-    
-elapsed = (time.time() - start)    
-print("Time: " + str(round(elapsed,2)) + " seconds")
+
+elapsed = (time.time() - start)
+print("Time: " + str(round(elapsed, 2)) + " seconds")
