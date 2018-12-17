07c61ff5f0d6b1e56a6c4fa15ff6d941f6fbe20a
Ben <benjamin.phillips22@gmail.com>
2018-12-17 13:25:08 +1100

    optimised the primes

--- p27.py
+++ p27.py
@@ -3,12 +3,16 @@
 @author: ben
 """
 # n = 71, a = -61, b = 971 and a*b = -59231
-# Time: 23.2843 seconds
+# Time: 5.3751 seconds
 
 import time
-import sympy
+from sympy import isprime
+from sympy import primerange
 
 start = time.time()
+
+b_list = list(primerange(0, 1000))
+our_primes = b_list.copy()
 
 
 def ab_tester(a, b):
@@ -17,14 +21,24 @@ def ab_tester(a, b):
     values of n, starting with n=0, for these a and b.
     """
     n = 0
-    while sympy.isprime(n**2 + a*n + b):
-        n += 1
-    return n
+    while True:
+        y = n**2 + a*n + b
+        
+        if y in our_primes:
+            n += 1
+            continue
+        
+        if isprime(y):
+            our_primes.append(y)
+            n += 1
+            continue
+        
+        return n
 
 
 best = {'a': 0, 'b': 0, 'n': 0}
 for a in range(-999, 1000):
-    for b in range(-1000, 1001):
+    for b in b_list:
         n = ab_tester(a, b)
         if n > best['n']:
             best['a'] = a
